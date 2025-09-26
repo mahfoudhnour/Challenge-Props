f4196e864a521368376eb4f4f9d17417582161e4
mahfoudhnour <mahfoudhnour58@gmail.com>
2025-09-26 22:31:23 +0200

    Create First Post

---
+++ src/FirstPost.js
@@ -0,0 +1,11 @@
+export default function FirstPost(age){
+    return(
+   <div>
+    <h1>{age}</h1>
+    <h2>Nour Mahfoudh</h2>
+    <hr/>
+    <p>CouCou bonjour comment cv ?</p>
+
+   </div>
+    );
+}
