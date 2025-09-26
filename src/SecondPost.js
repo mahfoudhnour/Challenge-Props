5eb2a662efbb8cee5236c593ae692937aa2ea274
mahfoudhnour <mahfoudhnour58@gmail.com>
2025-09-26 22:35:19 +0200

    Create Second Post

---
+++ src/SecondPost.js
@@ -0,0 +1,9 @@
+export default function SecondPost(){
+    return(
+        <div>
+            <h1>Hello world</h1>
+            <hr/>
+            <p>This is the hello world article</p>
+        </div>
+    );
+}
